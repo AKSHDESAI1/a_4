7041ea9a1dbd341a752fec69fd8230e37d5591df
AKSHDESAI1 <desaiaksh82@gmail.com>
2021-07-26 12:35:42 +0530

    a

--- b1/b1/urls.py
+++ b1/b1/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.b2, name='b2')
+    path('', views.b2, name='b2'),
+    path('rudra/', views.rudra, name='rudra')
 ]
--- b1/b1/views.py
+++ b1/b1/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def b2(request):
     return HttpResponse('<h1>wow </h1>')
+
+def rudra(request):
+    return HttpResponse('<h1>popat</h1>')
